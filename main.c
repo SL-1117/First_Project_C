e229ff55f57bfbfe0016b26a1780b8ae597b23dd
Li wei <liwei20051117@gmail.com>
2026-09-15 14:54:21 +0800

    Accidentally added Brother onto the program so I deleted it.

--- main.c
+++ main.c
@@ -8,7 +8,6 @@ int main() {
     printf("Dad!\n");
     printf("Mom!\n");
     printf("Sister!\n");
-    printf("Brother!\n");
     printf("I'm a new feature\n");
     printf("I'm the second feature\n");
 
